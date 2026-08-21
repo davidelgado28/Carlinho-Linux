3b0b3f0085d9cad1203c3d0808cba7c65f00ff2c
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-21 08:56:01 -0300

    Create busybox.sh for downloading and building BusyBox
    Add script to download and build BusyBox version 1.36.1

--- scripts/build/base/busybox.sh
+++ scripts/build/base/busybox.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
+set -e
 
+BUSYBOX_VER="1.36.1"
+wget https://busybox.net/downloads/busybox-${BUSYBOX_VER}.tar.bz2
+tar -xf busybox-${BUSYBOX_VER}.tar.bz2
+cd busybox-${BUSYBOX_VER}
+
+make defconfig
+# Para build estático sem dependências externas:
+sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
+make -j$(nproc)
+make CONFIG_PREFIX="../../sysroot" install
