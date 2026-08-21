2feef290b34aa016422370679f0c2ac40c9508de
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-21 09:33:24 -0300

    Update initrd.sh

--- scripts/initrd.sh
+++ scripts/initrd.sh
@@ -4,5 +4,8 @@ set -e
 INITRD_DIR="./initrd"
 OUTPUT="root/boot/initrd.img"
 
+chmod +x $INITRD_DIR/init
+
 cd "$INITRD_DIR"
 find . | cpio -o -H newc | gzip -9 > "../../$OUTPUT"
+echo "initrd.img gerado com sucesso!"
