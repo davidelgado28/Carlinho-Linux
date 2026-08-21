f551df22cb95fbe810c9cc2c62c508c5031bf916
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-21 09:04:12 -0300

    Update gcc.sh

--- scripts/build/base/gcc.sh
+++ scripts/build/base/gcc.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
+set -e
 
+GCC_VER="13.2.0"
+wget https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/gcc-${GCC_VER}.tar.xz
+tar -xf gcc-${GCC_VER}.tar.xz
+cd gcc-${GCC_VER}
+
+./contrib/download_prerequisites
+mkdir build && cd build
+../configure --prefix=/usr --disable-multilib --enable-languages=c,c++
+make -j$(nproc)
+make DESTDIR="$(pwd)/../../../sysroot" install
