81ab7a9ae131854135ff1d524aa081456886de73
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-21 09:03:48 -0300

    Add script to download and install kbd version 2.6.4

--- scripts/build/base/kbd.sh
+++ scripts/build/base/kbd.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+set -e
 
+KBD_VER="2.6.4"
+wget https://mirrors.edge.kernel.org/pub/linux/utils/kbd/kbd-${KBD_VER}.tar.xz
+tar -xf kbd-${KBD_VER}.tar.xz
+cd kbd-${KBD_VER}
+
+./configure --prefix=/usr --disable-vlock
+make -j$(nproc)
+make DESTDIR="$(pwd)/../../sysroot" install
