96d4cb8fd977e7392938e610cd0bfe56d552078c
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-21 08:56:36 -0300

    Add kernel build script for version 6.6.1

--- scripts/build/base/kernel.sh
+++ scripts/build/base/kernel.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+set -e
 
+KERNEL_VER="6.6.1"
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${KERNEL_VER}.tar.xz
+tar -xf linux-${KERNEL_VER}.tar.xz
+cd linux-${KERNEL_VER}
+
+make defconfig
+make -j$(nproc) bzImage
+cp arch/x86/boot/bzImage ../../root/boot/vmlinuz
