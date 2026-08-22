02928df5c419fa945d3299cebf69e7e28b5b69c5
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-21 21:09:03 -0300

    Update busybox.sh

--- scripts/build/base/busybox.sh
+++ scripts/build/base/busybox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-BUSYBOX_VER="1.36.4"
+BUSYBOX_VER="1.36.2"
 wget https://busybox.net/downloads/busybox-${BUSYBOX_VER}.tar.bz2
 tar -xf busybox-${BUSYBOX_VER}.tar.bz2
 cd busybox-${BUSYBOX_VER}
