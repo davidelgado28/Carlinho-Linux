870eb1bad689b66c35ddef687ef33a5678125012
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-21 21:18:52 -0300

    Update busybox.sh

--- scripts/build/base/busybox.sh
+++ scripts/build/base/busybox.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
 
-BUSYBOX_VER="1.36.3"
+BUSYBOX_VER="1.36.1"
 wget https://busybox.net/downloads/busybox-${BUSYBOX_VER}.tar.bz2
 tar -xf busybox-${BUSYBOX_VER}.tar.bz2
 cd busybox-${BUSYBOX_VER}
 
 make defconfig
 # Para build estático sem dependências externas:
+sed -i 's/CONFIG_TC=y/CONFIG_TC=n/' .config
 sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
 make -j$(nproc)
-make CONFIG_PREFIX="../../sysroot" install
+make CONFIG_PREFIX="../../sysroot" install 
